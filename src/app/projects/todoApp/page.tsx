c5a807648d295ff7b6f0a422df6364419c3e9081
cepeasbhy <asbhycepe01@gmail.com>
2023-08-24 11:58:50 +0800

    Fix for run build error

--- src/app/projects/todoApp/page.tsx
+++ src/app/projects/todoApp/page.tsx
@@ -1,66 +1,11 @@
-"use client";
-
 import "@/app/todo.css";
 import { Metadata } from "next";
-import { FormEvent, useState } from "react";
+import TodoApp from "./todoApp";
 
 export const metadata: Metadata = {
   title: "TODO APP",
 };
 
 export default function Page() {
-  const [taskList, setTaskList] = useState<string[]>([]);
-
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    const formData = new FormData(e.target as HTMLFormElement);
-    const taskItem = formData.get("task")!.toString().trim();
-
-    setTaskList([...taskList, taskItem]);
-  }
-
-  function deleteTask(selectedTask: number) {
-    const newTaskList = taskList.filter((_, index) => index != selectedTask);
-    setTaskList(newTaskList);
-  }
-  return (
-    <div id="container">
-      <div className="app-wrapper">
-        <div className="app-title">
-          <h2>TODO APP</h2>
-        </div>
-        <div className="main-app">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <div className="form-item">
-                <input
-                  type="text"
-                  name="task"
-                  required
-                  placeholder="Enter a task"
-                />
-              </div>
-              <div className="form-item">
-                <button>ADD TASK</button>
-              </div>
-            </div>
-          </form>
-          <div className="task-stats">
-            <p>Total Tasks: {taskList.length}</p>
-          </div>
-          <div className="display-tasks">
-            {taskList.map((item, index) => (
-              <div className="task-item" key={index}>
-                <div className="description">{item}</div>
-                <div className="action">
-                  <button onClick={() => deleteTask(index)}>DELETE</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <TodoApp />;
 }
